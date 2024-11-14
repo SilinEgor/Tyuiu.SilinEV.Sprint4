87eef69d35c297e0f8d16470bea6f850f1c1ee59
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-11-14 23:47:13 +0500

    Task6

---
+++ Tyuiu.SilinEV.Sprint4.Task6.V16.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.SilinEV.Sprint4.Task6.V16.Lib
+{
+    public class DataService : ISprint4Task6V16
+    {
+        public string[] Calculate(string[] array)
+        {
+            int a = 0;
+            string[] s = new string[a];
+
+            foreach (string d in array)
+            {
+                if (d.Length == 7)
+                {
+                    a++;
+                    Array.Resize(ref s, a);
+                    s[a - 1] = d;
+                }
+            }
+
+            return s;
+        }
+    }
+}
---
+++ Tyuiu.SilinEV.Sprint4.Task6.V16.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.SilinEV.Sprint4.Task6.V16.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
