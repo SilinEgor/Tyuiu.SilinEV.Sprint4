ff71c65dbf4901af7461829ae3a02de68b1ea7f8
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-11-14 23:16:46 +0500

    Task4

---
+++ Tyuiu.SilinEV.Sprint4.Task4.V3.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.ExceptionServices;
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.SilinEV.Sprint4.Task4.V3.Lib
+{
+    public class DataService : ISprint4Task4V3
+    {
+        public int Calculate(int[,] matrix)
+        {
+            int a = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] % 2 == 1)
+                    {
+                        a += matrix[i, j];
+                    }
+                }
+            }
+
+            return a;
+        }
+    }
+}
---
+++ Tyuiu.SilinEV.Sprint4.Task4.V3.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.SilinEV.Sprint4.Task4.V3.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
