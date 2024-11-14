f5d58ccd54a7cd203ea67d1458c83a180c44aec7
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-11-14 23:39:09 +0500

    Task5

---
+++ Tyuiu.SilinEV.Sprint4.Task5.V26.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.SilinEV.Sprint4.Task5.V26.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
