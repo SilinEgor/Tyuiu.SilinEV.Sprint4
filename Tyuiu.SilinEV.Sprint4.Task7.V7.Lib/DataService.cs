4385030aa3016a4531c3eb36e2011b464b819ede
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-11-14 23:55:47 +0500

    Task7

---
+++ Tyuiu.SilinEV.Sprint4.Task7.V7.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.SilinEV.Sprint4.Task7.V7.Lib
+{
+    public class DataService : ISprint4Task7V7
+    {
+        public int Calculate(int n, int m, string value)
+        {
+            int a = 0;
+            int[,] s = new int[4, 2];
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    int d = Convert.ToInt32(value[i * 2 + j]);
+                    s[i, j] = d;
+                    if (d % 2 == 1) a++;
+                }
+            }
+
+            return a;
+        }
+    }
+}
---
+++ Tyuiu.SilinEV.Sprint4.Task7.V7.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.SilinEV.Sprint4.Task7.V7.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
