12c00d5698b1f21e4829b5f22550d351a55e4788
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-11-14 22:16:21 +0500

    Task3

---
+++ Tyuiu.SilinEV.Sprint4.Task3.V18.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.SilinEV.Sprint4.Task3.V18.Lib
+{
+    public class DataService : ISprint4Task3V18
+    {
+        public int Calculate(int[,] array)
+        {
+            int a = 0;
+
+            for (int i = 0; i < array.GetLength(1); i++)
+            {
+                if (array[4, i] > a)
+                {
+                    a = array[4, i];
+                }
+            }
+
+            return a;
+        }
+    }
+}
---
+++ Tyuiu.SilinEV.Sprint4.Task3.V18.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.SilinEV.Sprint4.Task3.V18.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
